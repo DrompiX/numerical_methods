4138066d88d86f295c22af743d0c915fb0e5e808
Dima T <mrlocky98@gmail.com>
2018-03-05 14:22:57 +0300

    Simple chart and button were added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+
+import java.awt.event.ActionEvent;
+
 public class Controller {
+    @FXML LineChart<Number, Number> lineChart;
+
+    public void f(ActionEvent event) {
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+    }
 }
