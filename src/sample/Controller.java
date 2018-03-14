54c70d7706179766312864ff3978b89823aaa36f
Dima T <mrlocky98@gmail.com>
2018-03-14 22:47:41 +0300

    Small fixes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,22 +3,8 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
-
 import java.net.URL;
 import java.util.ResourceBundle;
-
-//public class Controller {
-//    @FXML LineChart<Number, Number> lineChart;
-//
-//    public void f(ActionEvent event) {
-//        XYChart.Series<Number, Number> series = new XYChart.Series<>();
-//    }
-//
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.chart.XYChart.Data;
 
@@ -27,13 +13,13 @@ public class Controller implements Initializable {
     @FXML
     private LineChart<Number, Number> MyChart;
 
-    // Variant 23 - y^2*e^x - 2y, y(1) = 0.5, x in [1, 7]
+    // Variant 23 - y^2*e^x - 2y, y(1.7) = -0.9025147, x in [1.7, 9]
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        double x0 = 1.7, y0 = initialFunction(x0), X = 10;
+        double x0 = 1.7, y0 = initialFunction(x0), X = 9;
         System.out.println(y0);
-        int N = 50;
+        int N = 30;
         double x[] = new double[N];
         double y[] = new double[N];
         double y2[] = new double[N];
@@ -46,12 +32,10 @@ public class Controller implements Initializable {
             x[i] = x[i - 1] + h;
         }
 
-//        for (int i = 0; i < N; i++) {
-//            y[i] = 3 * Math.pow(Math.E, 2 * x[i]) / (C - Math.pow(Math.E, 3 * x[i]));
-////            y[i] = 10.2871 * Math.pow(Math.E, -x[i]) / (10.2871 - Math.pow(Math.E, x[i]));
-//        }
         for (int i = 0; i < N - 1; i++) {
+            // Euler
             y[i + 1] = y[i] + h * (Math.pow(y[i], 2) * Math.exp(x[i]) + 2 * y[i]);
+            // Real
             y2[i] = initialFunction(x[i]);
         }
         y2[N - 1] = initialFunction(x[N - 1]);
@@ -72,7 +56,7 @@ public class Controller implements Initializable {
     }
 
     private double initialFunction(double x) {
-        final double C = 64.4199;
+        final double C = 64.4198735;
         return 3 * Math.pow(Math.E, 2 * x) / (C - Math.pow(Math.E, 3 * x));
     }
 }
